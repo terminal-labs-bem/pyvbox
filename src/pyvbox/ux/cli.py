7ff805d4ae61bef6b8d7711f7d9eebeb9f444a75
verhulstm <michael@terminallabs.com>
2024-04-27 04:34:29 -0500

    nice save

--- src/pyvbox/ux/cli.py
+++ src/pyvbox/ux/cli.py
@@ -1,6 +1,10 @@
+import io
+import os
 import shlex
+import shutil
 import subprocess
 import sys
+import tarfile
 from subprocess import Popen, PIPE
 
 import toml
@@ -18,6 +22,38 @@ def callshell(cmd):
     process = Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     return stdout.decode("utf-8").rstrip()
+    
+def get_file_progress_file_object_class():
+    class FileProgressFileObject(tarfile.ExFileObject):
+        def read(self, size, *args):
+            return tarfile.ExFileObject.read(self, size, *args)
+    return FileProgressFileObject
+
+class ProgressFileObject(io.FileIO):
+    def __init__(self, path, *args, **kwargs):
+        self._total_size = os.path.getsize(path)
+        io.FileIO.__init__(self, path, *args, **kwargs)
+
+    def read(self, size):
+        progressevent(self.tell(), self._total_size, "bar")
+        return io.FileIO.read(self, size)
+
+
+from time import sleep
+def progressbar(v, it, prefix="", size=60, out=sys.stdout):
+    count = len(it)
+    def show(j):
+        x = int(size*j/count)
+        print("{}[{}{}] {}/{}".format(prefix, "#"*x, "."*(size-x), j, count), 
+                end='\r', file=out, flush=True)
+    show(v)
+
+def progressevent(p, t, style):
+    if style == "bar":
+        progressbar(p, range(t), prefix="", size=60, out=sys.stdout)
+
+def sshquickcall():
+    pass
 
 def init():
     app.main()
@@ -35,20 +71,7 @@ def cli(ctx):
 @cli.command("init")
 def click_init_cmd():
     data = {
-      "target": {
-        "ip": "xx.xx.xx.xx",
-        "os": {
-          "os": "win 10",
-          "Arch": "x64"
-        },
-        "ports": {
-          "ports": ["1", "2"],
-          "1": {
-            "service": "xxx",
-            "ver": "5.9",
-          }
-        } 
-      }
+      "name": "testvm",
     }
     toml_string = toml.dumps(data)  # Output to a string
     output_file_name = "output.toml"
@@ -58,11 +81,49 @@ def click_init_cmd():
     
 @cli.command("up")
 def click_up_cmd():
-    print("up")
+    tarfile.TarFile.fileobject = get_file_progress_file_object_class()
+    tar = tarfile.open(fileobj=ProgressFileObject("/home/user/.pyvbox/boxes/platform.tar"))
+    tar.extractall("/home/user/.pyvbox/boxes/platform")
+    tar.close()
+    print("")
+    shutil.move("/home/user/.pyvbox/boxes/platform/platform", "/home/user/VirtualBox VMs")
+    callshell("vboxmanage registervm '/home/user/VirtualBox VMs/platform/platform.vbox'")
+    sleep(5)
+    callshell("vboxmanage sharedfolder remove 'platform' --name pyvbox")
+    callshell("vboxmanage sharedfolder add 'platform' --name pyvbox --hostpath '/home/user/Desktop/oracle'")
+    sleep(5)
+    callshell("vboxmanage modifyvm 'platform' --natpf1 'SSH,tcp,127.0.0.1,2522,10.0.2.15,22'")
+    sleep(5)
+    callshell("vboxmanage startvm platform")
+    sleep(5)
+    while True:
+        sleep(1)
+        if callshell("sshpass -p 'fjdksla;' ssh -p 2522 user@127.0.0.1 'ls'"):
+            print("ssh seems fine")
+            break
     
-@cli.command("down")
-def click_down_cmd():
-    print("down")
+    callshell("sshpass -p 'fjdksla;' ssh -p 2522 user@127.0.0.1 'sudo mkdir -p /pyvbox'")
+    callshell("sshpass -p 'fjdksla;' ssh -p 2522 user@127.0.0.1 'sudo mount -t vboxsf -o uid=1000,gid=1000 pyvbox /pyvbox'")
+    command = shlex.split("sshpass -p 'fjdksla;' ssh -p 2522 user@127.0.0.1 'cd /pyvbox; sudo bash provision.sh'")
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    )
+    for line in process.stdout:
+        sys.stdout.write(str(line, 'utf-8'))
+
+@cli.command("ssh")
+def click_ssh_cmd():
+    os.system("sshpass -p 'fjdksla;' ssh -p 2522 user@127.0.0.1")
+    
+@cli.command("destroy")
+def click_destroy_cmd():
+    callshell("vboxmanage controlvm 'platform' poweroff")
+    callshell("vboxmanage unregistervm 'platform' --delete")
+
+
+@cli.command("halt")
+def click_halt_cmd():
+    print("halt")
     
 @click.group(name="list")
 def list_group():
